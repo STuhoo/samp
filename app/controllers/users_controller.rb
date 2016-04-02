40404b1ee15ec81b1fdf1e33927c5f823396a79b
Tuhoo C <supertuhoo@163.com>
2016-04-01 23:52:48 -0400

    create new user with email, show information of user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new #define in C, used in V.(new.html.erb)
   end
   def show
     @user = User.find(params[:id]) #why only use id? only find id?
+    #define in C, used in V.(show.html.erb)
   end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render 'new'
+    end
+  end
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -19,6 +19,25 @@ describe "User pages" do
     before { visit signup_path }  # call to sign up URL
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) } 
+    
+    let(:submit) { "Create my account" }
+    describe "with vaild information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+    
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
   end
   
   describe "profile page" do
